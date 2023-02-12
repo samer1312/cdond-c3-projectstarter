470ce2e45ff5173c5e4273070bba1341404d5ae1
samer1312 <samer_zidan1312@hotmail.com>
2023-02-13 00:10:50 +0200

    final

--- .circleci/ansible/roles/configure-server/files/install_nvm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo su -c 'curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash' &>/dev/null
-sudo su ubuntu -c 'curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash' &>/dev/null
-#fix npm not found issue 
-sudo -i -u ubuntu bash << EOF
-source ~/.nvm/nvm.sh
-sleep 2
-nvm install 13.8.0 &>/dev/null
-EOF

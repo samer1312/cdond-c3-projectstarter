1daa96864c16545c45d3f3bd74412cbf50445c4f
samer1312 <samer_zidan1312@hotmail.com>
2023-02-13 09:16:17 +0200

    major change 354

--- .circleci/ansible/roles/install-node-npm/files/fix_it.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cp ~ubuntu/.bashrc ~ubuntu/.bashrc_orig
-cp ~ubuntu/.bashrc ~ubuntu/.bashrc_1
-tail -n 3 ~ubuntu/.bashrc > ~ubuntu/.bashrc_1
-for i in {1..3}; do sed -i '$d' ~ubuntu/.bashrc; done
-cat ~ubuntu/.bashrc >> ~ubuntu/.bashrc_1
-mv ~ubuntu/.bashrc ~ubuntu/.bashrc_old
-mv ~ubuntu/.bashrc_1 ~ubuntu/.bashrc
